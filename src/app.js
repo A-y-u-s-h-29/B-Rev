24c2d33d03c0031b7d7384747ee3b3a0e55f1748
Ayush Sharma <ayushsharma00123456@gmail.com>
2026-06-10 20:52:18 +0530

    change's

--- src/app.js
+++ src/app.js
@@ -26,13 +26,13 @@ app.delete("/notes/:id",(req,res)=>{
     })
 })
 
-// app.patch("/notes/:id",(req,res)=>{
-//     const id = req.params.id
-//     const titel = req.body.titel
-//     notes[id].titel = titel
-//     res.status(202).json({
-//         message:"Note Updated"
-//     })
-// })
+app.patch("/notes/:id",(req,res)=>{
+    const id = req.params.id
+    const titel = req.body.titel
+    notes[id].titel = titel
+    res.status(202).json({
+        message:"Note Updated"
+    })
+})
 
 module.exports = app;
